f53c6ab05229ed0060bbd671ee69b9e6073c38f9
Deem <Deem@DEEM>
2022-06-17 10:37:43 +0300

    Добавьте файлы проекта.

---
+++ ListViewSort/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewSort
+{
+    internal class User
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public int age { get; set; }
+
+        public User(string name, string country, int age)
+        {
+            this.name = name;
+            this.country = country;
+            this.age = age;
+        }
+
+    }
+}
